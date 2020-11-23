2f39c033269286f56cd80efa7d3a28717741c5f7
Kovacs <ekovacs@techteam.com>
2020-11-23 20:16:57 +0200

    added coap scaffolding

---
+++ client/index.js
@@ -0,0 +1,16 @@
+const coap = require('coap');
+
+setInterval(() => {
+
+    // Send some dummy data
+    const temp = Math.floor(Math.random() * (21.5 - 19.5) + 19.5);
+    const req = coap.request(`coap://[::1]/api/temp/${temp}`);
+    req.on('response', function(res) {
+        res.pipe(process.stdout)
+        res.on('end', function() {
+            // process.exit(0);
+        })
+    });
+    req.end();
+
+}, 3000);
