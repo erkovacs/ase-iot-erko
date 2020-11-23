5e052c8875063bae8658a7dd57756361e2be0f7c
Kovacs <ekovacs@techteam.com>
2020-11-23 20:39:13 +0200

    added mqtt scaffolding too

---
+++ ase-mqtt-erko/client/index.js
@@ -0,0 +1,18 @@
+const mqtt = require('mqtt');
+const client  = mqtt.connect('mqtt://test.mosquitto.org');
+ 
+client.on('connect', () => {
+  client.subscribe('presence', err => {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt');
+    } else {
+      console.error(err);
+    }
+  });
+});
+ 
+client.on('message', (topic, message) => {
+  // message is Buffer
+  console.log(message.toString());
+  client.end();
+})
