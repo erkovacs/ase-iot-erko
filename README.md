21c62977d55a69f97737b0b68c6d96f2c3d08f09
Kovacs <ekovacs@techteam.com>
2020-11-24 13:30:41 +0200

    moved to mqtt over ssl

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ASE IOT Projects
-TODO:: add docs 
+
+## For ASE-MQTT-ERKO
+TODO:: Not yet done
+
+## For ASE-COAP-ERKO
+MQTT client can be run using the following steps:
+ - Create tour key and cert in the `./ase-mqtt-erko` directory: `openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout client-key.pem -out client-cert.pem`
+ - Download Mosquitto ceritificate from `https://test.mosquitto.org/` and save to the same directory
+ - Or, just use the ones provided in the repo
+ - Run `npm run client`
--- ase-mqtt-erko/client/index.js
+++ ase-mqtt-erko/client/index.js
@@ -1,19 +1,38 @@
 const mqtt = require('mqtt');
-const client  = mqtt.connect('mqtt://test.mosquitto.org');
- 
+const fs = require('fs');
+
+const options = {
+  // Port according to spec on mosquitto website
+  port: 8884,
+
+  // Necessary only if the server requires client certificate authentication
+  key: fs.readFileSync('./client-key.pem'),
+  cert: fs.readFileSync('./client-cert.pem'),
+
+  // Necessary only if the server uses a self-signed certificate
+  ca: [ fs.readFileSync('./mosquitto.org.crt') ],
+
+  // Necessary only if the server's cert isn't for "localhost"
+  checkServerIdentity: () => { return null; }
+};
+
+const client  = mqtt.connect('mqtts://test.mosquitto.org', options);
+
 client.on('connect', () => {
+  
   client.subscribe('temp', err => {
-    if (!err) {
-      const temp = Math.floor(Math.random() * (21.5 - 19.5) + 19.5);
-      client.publish('temp', temp.toString());
-    } else {
-      console.error(err);
+    if (err) { 
+      console.log(err);
+      return;
     }
+
+    const temp = Math.floor(Math.random() * (21.5 - 19.5) + 19.5);
+    client.publish('temp', temp.toString());
   });
 });
  
 client.on('message', (topic, message) => {
   // message is Buffer
-  console.log(message.toString());
+  console.log('Response', message.toString());
   client.end();
-})
+});
